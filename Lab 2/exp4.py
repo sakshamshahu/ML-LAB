8234941a4fbec906656c4800d8b3a68bcc2e9c85
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-02-12 11:41:09 +0530

    folder structure

---
+++ Lab 2/exp4.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+#import statsmodel as sm using which you can create variables x and y 
+dataset = pd.read_csv('datasets/Summary of Weather.csv')
+
+X = np.array(dataset['MinTemp']).reshape(-1,1)
+print(X)
+y = np.array(dataset['MaxTemp']).reshape(-1,1)
+
+modelAlgo =  LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+modelAlgo.fit(X_train, y_train)
+
+print('The model score is: ',(modelAlgo.score(X_test, y_test))*100, '%')
+y_pred = modelAlgo.predict(X_test)
+plt.scatter(X_test, y_test, color ='g', marker='*')
+plt.plot(X_test, y_pred, color ='r')
+
+plt.show()
