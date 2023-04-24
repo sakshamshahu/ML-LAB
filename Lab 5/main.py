501194ae389e0074e950b3760d252f2b31dee650
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-04-24 10:23:28 +0530

    feat: correlation check between variables

--- Lab 5/main.py
+++ Lab 5/main.py
@@ -10,3 +10,5 @@ print("The max value of Sepal Length is", dataset['SepalLengthCm'].max(), "& the
 print("The max value of Sepal Width is", dataset['SepalWidthCm'].max(), "& the min value of Sepal Width is", dataset['SepalWidthCm'].min())
 print("The max value of Petal Length is", dataset['PetalLengthCm'].max(), "& the min value of Petal Length is", dataset['PetalLengthCm'].min())
 print("The max value of Petal Width is", dataset['PetalWidthCm'].max(), "& the min value of Petal Width is", dataset['PetalWidthCm'].min())
+
+print(dataset.corr())
